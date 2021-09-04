3b4fdca87ae2bcee58bf2891b82897fbf5dde7f4
Imerzel <zach.imerzel@gmail.com>
2021-09-04 13:36:55 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      {/* <Footer></Footer> */}
+    </div>
+
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Nav from './Nav'
+
+function Header() {
+    return(
+        <div>
+            <div>
+                <h1>Connect</h1>
+            </div>
+            <div>
+                {/* <Nav /> */}
+            </div>
+        </div>
+    )
+}
+
+export default Header;
