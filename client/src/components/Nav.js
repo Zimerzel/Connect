d1918f4b1e38108029c55f656f0ab08fa082ae85
Imerzel <zach.imerzel@gmail.com>
2021-09-10 10:52:40 -0500

    fixed nav a area

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,6 +1,4 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-// import { Navbar, Container, NavDropdown, Nav, NavItem } from "react-bootstrap"
 
 function Navbar(props) {
   return (
@@ -13,34 +11,3 @@ function Navbar(props) {
 }
 
 export default Navbar;
-
-       /* <Navbar variant="dark" bg="dark" expand="lg">
-      <Container fluid>
-        <Navbar.Brand as={Link} to='/'>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-dark-example" />
-        <Navbar.Collapse id="navbar-dark-example">
-          <Nav>
-            <NavDropdown
-              id="nav-dropdown-dark-example"
-              title="Menu"
-              menuVariant="dark"
-            >
-              <NavDropdown.Item href="/">home</NavDropdown.Item>
-              <NavDropdown.Item href="/about">about</NavDropdown.Item>
-              <NavDropdown.Item href="/learn">Learn</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar> *}
-
-    // <div className="nav row" id="nav">
-    //     <NavLink to="/">Home </NavLink>
-    //     <NavLink to="/about">About </NavLink>
-    //     <NavLink to="/learn">Learn </NavLink>
-    // </div>
-
-    // Create a dropdown icon menu
-  );
-} */
--- client/src/components/NavDropdown.js
+++ client/src/components/NavDropdown.js
@@ -3,6 +3,13 @@ import { ReactComponent as Caret } from '../icons/caret.svg'
 import { ReactComponent as Messenger } from '../icons/messenger.svg'
 import { CSSTransition } from "react-transition-group"
 import { ReactComponent as  Arrow} from '../icons/arrow.svg'
+import { ReactComponent as  Home} from '../icons/home.svg'
+import { ReactComponent as  Article} from '../icons/article.svg'
+import { ReactComponent as  Community} from '../icons/community.svg'
+import { ReactComponent as  Books} from '../icons/books.svg'
+import { ReactComponent as  Video} from '../icons/video.svg'
+import { ReactComponent as  Signup} from '../icons/signup.svg'
+import { ReactComponent as  Cog} from '../icons/cog.svg'
 
 
 function DropdownMenu () {
@@ -37,9 +44,8 @@ function DropdownMenu () {
                 classNames="menu-primary" 
                 onEnter={calcHeight}>
                 <div className="menu">
-                    {/* <DropdownItem leftIcon={<Arrow />} exit to current page/> */}
                     <DropdownItem
-                        // leftIcon={<Caret />} make it a home icon
+                        leftIcon={<Home />}
                     >
                         <a href="/">Home</a>
                     </DropdownItem>
@@ -54,6 +60,12 @@ function DropdownMenu () {
                         goToMenu="learn">
                             <a href="/learn">Learn</a>
                     </DropdownItem>
+                    <DropdownItem
+                        href ="/signup"
+                        leftIcon={<Cog />}
+                    >
+                        <a href="/signup">Signup</a>
+                    </DropdownItem>
                 </div>
             </CSSTransition>
             <CSSTransition 
@@ -64,7 +76,7 @@ function DropdownMenu () {
                 onEnter={calcHeight}>
                 <div className="menu">
                     <DropdownItem leftIcon={<Arrow />} goToMenu="main" />
-                    <DropdownItem>
+                    <DropdownItem leftIcon={<Community />}>
                        <a href="/about/community">Community</a>
                     </DropdownItem>
                     <DropdownItem
@@ -82,13 +94,13 @@ function DropdownMenu () {
                 onEnter={calcHeight}>
                 <div className="menu">
                     <DropdownItem leftIcon={<Arrow />} goToMenu="main" />
-                    <DropdownItem >
+                    <DropdownItem leftIcon={<Books />}>
                         <a href="/learn/books">Books</a>
                     </DropdownItem>
-                    <DropdownItem>
+                    <DropdownItem leftIcon={<Video />}>
                         <a href="/learn/videos">Videos</a>
                     </DropdownItem>
-                    <DropdownItem>
+                    <DropdownItem leftIcon={<Article />}>
                         Articles
                     </DropdownItem>
                 </div>
