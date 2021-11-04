aeaa5dde70e0d826a461e0b448421b99a186adc8
Imerzel <zach.imerzel@gmail.com>
2021-11-03 21:12:53 -0500

    base tiny page added, needs css

--- src/components/Tiny.js
+++ src/components/Tiny.js
@@ -59,6 +59,23 @@ const Tiny = () => {
                     <Row>
                     <Col>
                         <div id="why-tiny">
+                            <h2> What is the Tiny House Movement?</h2>
+                            <p>
+                            The Tiny House Movement is an architectural and social
+                            movement that encourages living a simpler life in a smaller space.
+                            People from all walks of life have determined that a large home,
+                            and more specifically, the large cost of living that comes with 
+                            it, is both unnecessary and a detriment to their happiness. 
+                            These people have turned to tiny house living to reduce the 
+                            financial and emotional burden of STUFF.
+                            </p>
+                            <p>
+                            A large percentage of people involved in the tiny house movement are DIY’ers,
+                            meaning people who are interested in building their own homes. It is incredibly 
+                            empowering and fulfilling to construct your own home from the ground up. With 
+                            that said, as the tiny house movement becomes more mainstream, more companies 
+                            are trying to capitalize on the increased demand by offering pre-built and custom homes.
+                            </p>
                             <h2> Why People are Going Tiny </h2>
                             <p>
                             Over the years we’ve talked to tens of thousands of 
@@ -102,53 +119,135 @@ const Tiny = () => {
                             many benefits beyond economical.
                             </p>
                             <h3> Simplification </h3>
-                                <p>
+                            <p>
                                 There is no need to move into a tiny house if simplifying your life is your goal. 
                                 There are techniques that can be used to simplify now. There is no need to wait. 
                                 While this is true, a tiny house will force you to lead a simpler life. 
-                                </p>
-                                <p>
+                            </p>
+                            <p>
                                 Living tiny results in owning less. With that comes less thinking about your stuff, 
                                 less time upgrading your stuff, less time maintaining your stuff, you get the picture. 
                                 This process is so common that we’ve stopped noticing it and just consider it part of life.
                                 It is an invisible weight on our shoulders. Only after it is removed do we recognize that it 
                                 even existed. When I talk to people that have moved into tiny homes, I am repeatedly told 
                                 how surprised they are that something they didn’t even know existed was having such a big impact on them.
-                                </p>
-                                <p>
+                            </p>
+                            <p>
                                 Financial reasons are what brings most people to the tiny house movement, but the simpler life is what keeps them in it.
-                                </p>
+                            </p>
 
-                                <h3> Enviromental Benifits </h3>
-                                <p>
+                            <h3> Enviromental Benifits </h3>
+                            <p>
                                 Using fewer utilities not only saves money but it also has a smaller impact on the environment. Some homes go so far as to 
                                 use no utilities by being completely off-grid. Also, with less consumption comes less waste going into landfills.
-                                </p>
+                            </p>
 
-                                <h2>Challenges of Going Tiny</h2>
-                                <p>
+                            <h2>Challenges of Going Tiny</h2>
+                            <p>
                                 While there are significant advantages and benefits of joining the tiny house movement, there are also challenges 
                                 that need to be overcome before you take the plunge.
-                                </p>
-                                <h3>Where to Park your Tiny House</h3>
-                                <p>
+                            </p>
+                            <h3>Where to Park your Tiny House</h3>
+                            <p>
                                 Probably the biggest challenge that people face with transitioning to a tiny house is finding a legal place to put their home.
                                 Codes and laws change from state to state, county to county, and even city to city. This doesn’t make it easy to just pull in 
                                 and say, “I am going to buy this property and place my tiny house on it and live carefree!” Many places will tell you that you 
                                 cannot live in a vehicle on the property for more than 30 consecutive days (because they consider the tiny house an RV).
-                                </p>
-                                <h3> Financing and Insurance </h3>
-                                <p>
+                            </p>
+                            <h3> Financing and Insurance </h3>
+                            <p>
                                 Financing and insurance can be a challenge as well. Financing and insurance companies rely on having accurate values for the 
                                 items they finance and insure, and with tiny houses those values aren’t always reliable or clear. For instance, if an individual 
                                 built their own house and did so incorrectly, their structure could potentially be worthless.
-                                </p>
-                                <p>
+                            </p>
+                            <p>
                                 As the demand for tiny houses grows and the market becomes more mature, we are seeing more and more companies willing to take the
                                 risk with tiny houses. There are now big companies offering financing and insurance for them.
-                                </p>
+                            </p>
+
+                            <h2>Who's Joining the Tiny House Movement</h2>
+                            <p>
+                                While anyone can benefit from the advantages of living tiny,
+                                we primarily see two groups that are most interested; younger 
+                                individuals or couples that do not have children, and those entering retirement.
+                            </p>
+                            <p>
+                                There are many reasons younger people are more interested in tiny houses.
+                                There is risk associated with moving into a tiny house as highlighted above, 
+                                and the uncertainty of having a permanent spot for your home can be emotionally 
+                                challenging for anyone. But it is less of a concern for younger individuals who 
+                                can be more flexible and make faster lifestyle changes. Changes, such as moving,
+                                become increasingly difficult the more roots you have down, for example having 
+                                children in school.
+                            </p>
+                            <p>
+                                Younger people have also had less time to acquire more belongings, so that idea of minimizing is less impactful. 
+                                People tend to fill the space they are in, and if you have not yet lived in a home on your own, you likely won’t 
+                                have as much stuff as someone who has
+                            </p>
+                            <p>
+                                Those entering retirement are also more interested in tiny houses. As we age and look to spend less time working,
+                                our bills unfortunately don’t change because of our desires. To cover those bills and our cost of living, we either 
+                                need to have adequate savings or reduce our cost of living to a sustainable level. Unfortunately, many don’t have 
+                                the needed savings and are thus looking to the alternative. As previously outlined, one way to dramatically decease
+                                that cost of living is to go tiny. The biggest roadblock to retired individuals moving into a tiny house is the uncertainty 
+                                of a permanent living location. No one in retirement wants to be told they must move and go through the challenges of finding 
+                                a new location for their home.
+                            </p>
+                            <p>
+                                As you may have noticed, these two groups exclude families. While some families do indeed live in tiny houses, it is not as common.
+                                The challenges around having the room for the stuff people typically associate with having children, as well as privacy and personal 
+                                space limitations, is too much for many to take on.
+                            </p>
                         </div>
                     </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <div>
+                            <h2>Tiny House Movement Growth</h2>
+                            <p>
+                            Tiny houses have always been around in some form. 
+                            Mostly people just called them houses back when all 
+                            houses were tiny. But as homes became increasingly 
+                            larger our definition and concept of home changed.
+                            </p>
+                            <p>
+                            In 1999, Jay Shafer built one of the first tiny houses 
+                            on a trailer and jump started the modern tiny house movement. 
+                            While interest grew steadily over time it wasn’t until 2014 
+                            that the movement went mainstream. That was the year the show 
+                            ‘Tiny House Nation’ first aired and the term ‘Tiny House’ 
+                            entered most people’s vocabulary.
+                            </p>
+                        </div>
+                    </Col>
+                    <Col>
+                        <div>
+                            <img src="https://www.tinyhomebuilders.com/images/help/help-tiny-house-search-trend.webp" alt="Tiny house growth graph" />
+                        </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <div>
+                        <h2>Learn More and Get Involved</h2>
+                        <p>
+                        Tiny house supporters are gathering in groups, starting organizations, 
+                        and talking to the powers that be. You will be hard-pressed to find a more 
+                        passionate group of people then the tiny house community. That is why this 
+                        is more than a fad, more than a trend, it is truly a movement. Rarely if 
+                        ever have you heard of people coming together on a national level to fight 
+                        for other real estate trends. And the media is onboard. Reality shows on 
+                        television, articles in every major magazine and on every major website as 
+                        well as more news articles arriving daily.
+                        </p>
+                        <p>
+                        If you are considering transitioning to tiny living, look in your local area (or where you hope to live), 
+                        and see if a group already exists. This can be a great resource to find out what is allowed, and what is being pushed for locally. 
+                        One resource to find local groups is the Tiny House Map [tinyhousemap.com] or by searching the web for ‘tiny house’ and your cities name. 
+                        There is no need to reinvent the wheel and more can always be accomplished by a group than an individual.
+                        </p>
+                    </div>
                 </Row>
             </Container>
             </div>
